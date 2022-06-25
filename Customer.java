8f3cfa5357306f3040c4574e77c22c7368d0b9c6
Klysman <klysman.rk@gmail.com>
2022-06-25 23:29:04 +0000

    Commit 2

--- Customer.java
+++ Customer.java
@@ -26,22 +26,6 @@ public class Customer {
         double thisAmount = 0;
         Rental each = (Rental) rentals.nextElement();
 
-        //determine amounts for each line
-        switch (each.getMovie().getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (each.getDaysRented() > 2)
-                 thisAmount += (each.getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += each.getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (each.getDaysRented() > 3)
-                 thisAmount += (each.getDaysRented() - 3) * 1.5;
-               break;
-        }
 
         // add frequent renter points
         frequentRenterPoints ++;
@@ -61,4 +45,29 @@ public class Customer {
              " frequent renter points";
      return result;
    }
+  
+  private double amountFor(Rental each) {
+    //determine amounts for each line
+    double thisAmount = 0;
+    switch (each.getMovie().getPriceCode()) {
+       case Movie.REGULAR:
+          thisAmount += 2;
+          if (each.getDaysRented() > 2)
+             thisAmount += (each.getDaysRented() - 2) * 1.5;
+          break;
+       case Movie.NEW_RELEASE:
+          thisAmount += each.getDaysRented() * 3;
+          break;
+       case Movie.CHILDRENS:
+          thisAmount += 1.5;
+          if (each.getDaysRented() > 3)
+             thisAmount += (each.getDaysRented() - 3) * 1.5;
+           break;
+       }
+    return thisAmount;
+    }
+  
+
+  
+  
 }
